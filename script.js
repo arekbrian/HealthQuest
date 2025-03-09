5bceda87e263fba4f004b84b486e50cba68d26af
arekbrian <arekbrian@gmail.com>
2025-03-09 13:46:36 +0300

    second commit

---
+++ script.js
@@ -0,0 +1,7 @@
+{/* <script>
+    function playSound(soundFile) {
+        var audio = document.getElementById('sleep-audio');
+        audio.src = soundFile;
+        audio.play();
+    }
+</script> */}
